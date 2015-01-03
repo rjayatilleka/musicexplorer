cbd3f7ccd143b62742ec7009d604e3cca99ac8f6
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-01-03 17:05:56 -0600

    Watchify working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
-body = $('body');
-body.innerHTML += '<p>From JavaScript</p>';
+var body = $('body');
+body.innerHTML += '<p>From JavaScript, with Watchify</p>';
